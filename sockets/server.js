1ee0161c2d3e3cb2e9383fb35d8f356bd64d7ddc
Steve <stevegonzales807@gmail.com>
2020-02-11 17:25:17 -0500

    nuevo

---
+++ sockets/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+
+
+const http = require('http');
+
+const server = http.createServer(app);
+
+
+server.listen(1000);
+
+app.use(express.static('public'));
+
+const socketIo = require('socket.io');
+const io = socketIo.listen(server);
+
+io.on('connect',function(socket){
+    console.log("nueva conexion id: "+ socket.id);
+});
