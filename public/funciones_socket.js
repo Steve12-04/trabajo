32dda0fd9040d28380cfa7d5b53141fae26fb07a
Steve <stevegonzales807@gmail.com>
2020-02-14 11:52:41 -0500

    este es el avance papi :D

--- public/funciones_socket.js
+++ public/funciones_socket.js
@@ -2,19 +2,13 @@ const socket = io.connect();
         socket.on('nuevo_usuario',function(datos){
             alert("nuevo usuario conectado: "+datos.user);
         });
-
         socket.on('nuevo_mensaje',function(datos){
-            $('#nuevo_mensaje').append('<p ><strong>'+datos.user+': </strong>esto es un mensaje </p>')
+            $('#cont_mensajes').append('<p ><strong>'+datos.user+': </strong>'+datos.mensaje+' </p>')
         });
 
-function loguear(){
-    correo = $('#login_form #correo').val();
-    usuario = $('#login_form #usuario').val();
+function loguear(correo,usuario){
     socket.emit('datos_usuario', {correo: correo, usuario:usuario});
-    
 }
-function enviar_msj{
-    mensaje = $('#mensaje').val();
-    alert(mensaje);
+function enviar_msj(usuario,mensaje){    
     socket.emit('send_mensaje', {mensaje: mensaje, usuario: usuario});
 }
--- sockets/server.js
+++ sockets/server.js
@@ -19,11 +19,12 @@ io.on('connect',function(socket){
     console.log("nueva conexion id: "+ socket.id);
 
     socket.on('datos_usuario',function(datos){
-            console.log('correo: ' + datos.correo + ' usuario: ' + datos.usuario);
+            id_user=socket.id;
+            console.log('correo: ' + datos.correo + ' usuario: ' + datos.usuario+'id_socket: '+ id_user);
             io.emit('nuevo_usuario', {user:datos.usuario});
-    });
+        });
     socket.on('send_mensaje',function(datos){
-        console.log(datos.usuario + 'esta enviando un mensaje');
-        io.emit('nuevo_mensaje', {user:datos.usuario, mensaje: datos.mensaje});
-    
-});
+        console.log(datos.usuario + ' esta enviando un mensaje');
+        io.to(id_usuario).emit('nuevo_mensaje', {user:datos.usuario, mensaje: datos.mensaje});
+        });
+})
